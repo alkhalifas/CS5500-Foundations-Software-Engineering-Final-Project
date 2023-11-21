10e5b840947602beab4d249b825ee53a02a14e75
Saleh Alkhalifa <alkhalifas@thallium.lan>
2023-11-21 08:14:15 -0500

    added server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // ************** THIS IS YOUR APP'S ENTRY POINT. CHANGE THIS FILE AS NEEDED. **************
 // ************** DEFINE YOUR REACT COMPONENTS in ./components directory **************
-import './stylesheets/App.css';
+import './stylesheets/index.css';
 import FakeStackOverflow from './components/fakestackoverflow.js';
 import React from 'react';
 
