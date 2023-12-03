f8349560a2f45506eaac82a58d2206faa1b5b9ba
Saleh Alkhalifa <alkhalifas@thallium.lan>
2023-12-03 15:29:03 -0500

    adding tests for the application

--- testing/cypress/e2e/fakeso.cy.js
+++ testing/cypress/e2e/fakeso.cy.js
@@ -13,43 +13,36 @@ describe('Fake SO Test Suite', () => {
           cy.exec('node ../server/destroy.js');
 
       });
-    it('successfully shows page title', () => {
+
+    it('1.1 | Shows page title, welcome message, login, register, and guest options', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Fake Stack Overflow');
-    })
-    it('successfully shows page greeting', () => {
-        cy.visit('http://localhost:3000');
         cy.contains('Welcome, Please Select a Method to Proceed');
-    })
-    it('successfully shows log in button', () => {
-        cy.visit('http://localhost:3000');
         cy.contains('Log In');
-    })
-    it('successfully shows register button', () => {
-        cy.visit('http://localhost:3000');
         cy.contains('Register');
-    })
-    it('successfully shows guest button', () => {
-        cy.visit('http://localhost:3000');
         cy.contains('Proceed as Guest');
     })
-    it('successfully navigate to register page', () => {
+
+    it('1.2 | Navigate to register page', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Register').click();
     })
-    it('successfully shows log in button', () => {
+    it('1.3 | Navigate to log in page', () => {
+        cy.visit('http://localhost:3000');
+        cy.contains('Login').click();
+    })
+    it('1.4 | Proceed as guest', () => {
+        cy.visit('http://localhost:3000');
+        cy.contains('Proceed as Guest').click();
+    })
+    it('1.5 | Shows thee login options', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Log In');
-    })
-    it('successfully shows register button', () => {
-        cy.visit('http://localhost:3000');
         cy.contains('Register');
-    })
-    it('successfully shows guest button', () => {
-        cy.visit('http://localhost:3000');
         cy.contains('Proceed as Guest');
     })
-    it('successfully create a user and login and see home page', () => {
+
+    it('2.1 | Create a user and login and see home page and its associated items', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Register').click();
         cy.get('#usernameInput').type('someone')
@@ -58,7 +51,6 @@ describe('Fake SO Test Suite', () => {
         cy.get('#confirmPasswordInput').type('Password_123')
         cy.get('#registerSubmit').click();
 
-        cy.visit('http://localhost:3000');
         cy.get('#username').type('someone')
         cy.get('#password').type('Password_123')
         cy.contains('Log In').click();
@@ -75,7 +67,7 @@ describe('Fake SO Test Suite', () => {
         cy.contains('Tags').click();
 
     })
-    it('successfully create a user and login and answer a question', () => {
+    it('2.2 | Create a user and login and answer a question', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Register').click();
         cy.get('#usernameInput').type('alkhalifas')
@@ -92,9 +84,46 @@ describe('Fake SO Test Suite', () => {
         cy.contains('Question Title 10').click();
         cy.contains('Answer Question').click();
         cy.get('#answerTextInput').type("Unit Test Answer");
+        cy.contains('Unit Test Answer')
 
     })
-    it('successfully create a user and login and open ask question', () => {
+
+    it('2.3 | Log in, ask a question creates and displays expected meta data', () => {
+
+        cy.visit('http://localhost:3000');
+        cy.contains('Register').click();
+        cy.get('#usernameInput').type('alkhalifas')
+        cy.get('#emailInput').type('alkhalifas@gmail.com')
+        cy.get('#passwordInput').type('Password_123')
+        cy.get('#confirmPasswordInput').type('Password_123')
+        cy.get('#registerSubmit').click();
+
+        cy.visit('http://localhost:3000');
+        cy.get('#username').type('alkhalifas')
+        cy.get('#password').type('Password_123')
+        cy.contains('Log In').click();
+
+        cy.contains('Ask a Question').click();
+        cy.get('#formTitleInput').type('UNIT TEST Question Q1');
+        cy.get('#formTextInput').type('UNIT TEST Question Q1 Text T1');
+        cy.get('#formTagInput').type('Relativity');
+
+        cy.contains('Post Question').click();
+        cy.contains('Fake Stack Overflow');
+        cy.contains('11 Questions');
+        cy.contains('alkhalifas asked 0 seconds ago');
+        const answers = ['0 answers', '1 answers','2 answers', '3 answers', '2 answers'];
+        const views = ['0 views', '103 views', '200 views', '121 views','10 views'];
+        cy.get('.postStats').each(($el, index, $list) => {
+            cy.wrap($el).should('contain', answers[index]);
+            cy.wrap($el).should('contain', views[index]);
+        });
+        cy.contains('Unanswered').click();
+        cy.get('.postTitle').should('have.length', 1);
+        cy.contains('1 question');
+    })
+
+    it('2.4 | Creates a user and logs in and asks a question with exiting tag', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Register').click();
         cy.get('#usernameInput').type('alkhalifas')
@@ -115,9 +144,30 @@ describe('Fake SO Test Suite', () => {
         cy.contains('Post Question').click();
 
         cy.contains('11 questions');
-
     })
-    it('successfully register, login and see tags page', () => {
+    it('2.5 | Creates a user and logs in and asks a question with new tag without enough rep', () => {
+        cy.visit('http://localhost:3000');
+        cy.contains('Register').click();
+        cy.get('#usernameInput').type('alkhalifas')
+        cy.get('#emailInput').type('alkhalifas@gmail.com')
+        cy.get('#passwordInput').type('Password_123')
+        cy.get('#confirmPasswordInput').type('Password_123')
+        cy.get('#registerSubmit').click();
+
+        cy.visit('http://localhost:3000');
+        cy.get('#username').type('alkhalifas')
+        cy.get('#password').type('Password_123')
+        cy.contains('Log In').click();
+
+        cy.contains('Ask a Question').click();
+        cy.get('#formTitleInput').type('Unit Test Question Title');
+        cy.get('#formTextInput').type('Unit Test Question Text');
+        cy.get('#formTagInput').type('relativity');
+        cy.contains('Post Question').click();
+
+        cy.contains('Only a user with reputation of 50 or more can create a new tag');
+    })
+    it('2.6 successfully register, login and see tags page', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Register').click();
         cy.get('#usernameInput').type('alkhalifas')
@@ -135,7 +185,7 @@ describe('Fake SO Test Suite', () => {
         cy.contains('8 Tags');
     })
 
-    it('successfully register, login and see profile page', () => {
+    it('2.7 successfully register, login and see profile page', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Register').click();
         cy.get('#usernameInput').type('alkhalifas')
