2f7e6f74023dba184fa2d94a61dbe2f047c9c465
Saleh Alkhalifa <alkhalifas@thallium.lan>
2023-12-03 10:07:56 -0500

    init added

--- server/init.js
+++ server/init.js
@@ -1,1 +1,81 @@
-// Setup database with initial test data.
+// init.js
+
+const mongoose = require('mongoose');
+const User = require('./models/users');
+const Question = require('./models/questions');
+const Answer = require('./models/answers');
+const Tag = require('./models/tags');
+const bcrypt = require('bcrypt');
+
+let mongoDB = 'mongodb://localhost:27017/fake_so'; // Update with your MongoDB URL
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const sampleUsers = [
+    { name: 'Werner Heisenberg', username:"wheisenberg", email: 'wheisenberg@science.com', password: "Password_123" },
+    { name: 'Niels Bohr', username:"nbohr", email: 'nbohr@science.com', password: "Password_123" },
+    { name: 'Ernest Rutherford', username:"eruth", email: 'eruth@example.com', password: "Password_123" },
+    { name: 'JJ Thompson', username:"jjthom", email: 'jjthom@example.com', password: "Password_123" },
+    { name: 'John Dalton', username:"jdalt", email: 'jdalt@example.com', password: "Password_123" },
+    { name: 'Albert Einstein', username:"aeinstein", email: 'aeinstein@example.com' , password: "Password_123"},
+    { name: 'Erwin Schrodinger', username:"eschrod", email: 'eschrod@example.com' , password: "Password_123"}
+];
+
+const tags = [
+    'Quantum Mechanics', 'Relativity', 'Atomic Structure', 'Electron Discovery',
+    'Nuclear Physics', 'Quantum Theory', 'Physics', 'Mathematics'
+];
+
+async function createUser(userData) {
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    let user = new User({ ...userData, password: hashedPassword });
+    return user.save();
+}
+
+async function createTag(tagName) {
+    let tag = new Tag({ name: tagName });
+    return tag.save();
+}
+
+async function createAnswer(text, user) {
+    let answer = new Answer({ text: text, ans_by: user.username });
+    return answer.save();
+}
+
+async function createQuestion(title, text, tagIds, answerIds, user) {
+    let question = new Question({
+        title,
+        text,
+        tags: tagIds,
+        answers: answerIds,
+        asked_by: user.username
+    });
+    return question.save();
+}
+
+async function populate() {
+    try {
+        await db.dropDatabase(); // drop to avoid cluttering dbs
+
+        let createdUsers = await Promise.all(sampleUsers.map(user => createUser(user)));
+        let createdTags = await Promise.all(tags.map(tag => createTag(tag)));
+
+        // 2 answers per question
+        for (let i = 0; i < 10; i++) {
+            let questionUser = createdUsers[i % createdUsers.length];
+            let answer1 = await createAnswer(`Answer 1 for question ${i + 1}`, questionUser);
+            let answer2 = await createAnswer(`Answer 2 for question ${i + 1}`, questionUser);
+            let questionTags = [createdTags[i % createdTags.length], createdTags[(i + 1) % createdTags.length]];
+            await createQuestion(`Question Title ${i + 1}`, `Question Text ${i + 1}`, questionTags.map(tag => tag._id), [answer1._id, answer2._id], questionUser);
+        }
+
+        console.log('Database seeded successfully');
+    } catch (error) {
+        console.error('Error seeding database:', error);
+    } finally {
+        mongoose.connection.close();
+    }
+}
+
+populate();
--- testing/cypress/e2e/fakeso.cy.js
+++ testing/cypress/e2e/fakeso.cy.js
@@ -3,18 +3,107 @@ describe('Fake SO Test Suite', () => {
     beforeEach(() => {
         // Seed the database before each test
 //        cy.exec('node /path/to/server/init.js');
-      });
+        cy.exec('node ../server/init.js');
+
+    });
 
       afterEach(() => {
         // Clear the database after each test
 //        cy.exec('node /path/to/server/destroy.js');
+          cy.exec('node ../server/destroy.js');
+
       });
-    it('successfully shows All Questions string', () => {
+    it('successfully shows page title', () => {
         cy.visit('http://localhost:3000');
-        cy.contains('All Questions');
-    });
-    it('successfully shows Ask a Question button', () => {
+        cy.contains('Fake Stack Overflow');
+    })
+    it('successfully shows page greeting', () => {
         cy.visit('http://localhost:3000');
-        cy.contains('Ask a Question');
-    });
+        cy.contains('Welcome, Please Select a Method to Proceed');
+    })
 })
+//
+// describe('Login Test 1', () => {
+//     it('successfully shows page title', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Fake Stack Overflow');
+//     })
+// })
+//
+// describe('Login Test 2', () => {
+//     it('successfully shows page greeting', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Welcome, Please Select a Method to Proceed');
+//     })
+// })
+//
+// describe('Login Test 3', () => {
+//     it('successfully shows log in button', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Log In');
+//     })
+// })
+//
+// describe('Login Test 4', () => {
+//     it('successfully shows register button', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Register');
+//     })
+// })
+//
+// describe('Login Test 5', () => {
+//     it('successfully shows guest button', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Proceed as Guest');
+//     })
+// })
+//
+// describe('Login Test 6', () => {
+//     it('successfully navigate to register page', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Register').click();
+//     })
+// })
+//
+// describe('Register Test 1', () => {
+//     it('successfully shows log in button', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Log In');
+//     })
+// })
+//
+// describe('Register Test 2', () => {
+//     it('successfully shows register button', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Register');
+//     })
+// })
+//
+// describe('Register Test 3', () => {
+//     it('successfully shows guest button', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Proceed as Guest');
+//     })
+// })
+//
+// describe('Register Test 4', () => {
+//     it('successfully navigate to register page', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Register').click();
+//         cy.get('#usernameInput').type('someone')
+//         cy.get('#emailInput').type('someone@gmail.com')
+//         cy.get('#passwordInput').type('Password_123')
+//         cy.get('#confirmPasswordInput').type('Password_123')
+//         cy.get('#registerSubmit').click();
+//
+//     })
+// })
+//
+// describe('Login Test 6', () => {
+//     it('successfully navigate to register page', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Log In').click();
+//     })
+// })
+//
+//
